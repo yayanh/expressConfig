af23dfb4294590b4453fb1bd789b9f1a3a4fb4cc
yayanh <fish_d@aliyun.com>
2016-12-26 08:46:54 +0800

    添加静态文件路由

--- app/main.js
+++ app/main.js
@@ -1,5 +1,8 @@
 "use strict";
 var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+//不显示引入，你会得到"Uncaught reflect-metadata shim is required when using class decorators"的错误
+require("reflect-metadata");
+require("zone.js/dist/zone");
 var app_module_1 = require("./app.module");
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
